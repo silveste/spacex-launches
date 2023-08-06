ab46a13b204b4b7f7e230da01bf420b41fa117f1
Antonio Fernandez <silveste@mailbox.org>
2023-08-06 23:53:33 +0100

    feat: add layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,21 @@ import { Container } from "@mui/material";
 export default function Home() {
   return (
     <Container maxWidth="lg" sx={{ backgroundColor: "red" }}>
-      SpaceX Launch
+      <Box
+        component="header"
+        display="flex"
+        justifyContent="space-between"
+        sx={{ backgroundColor: "green" }}
+      >
+        <h1>SpaceX Launches</h1>
+        <select>Filters</select>
+      </Box>
+      <Box component="main" minHeight="80vh" backgroundColor="yellow">
+        Bingo
+      </Box>
+      <Box component="footer" backgroundColor="blue">
+        footer
+      </Box>
     </Container>
   );
 }
